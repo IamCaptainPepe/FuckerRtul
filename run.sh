7f601a91185be3da277c9fcad70ef652de7cf67f
CaptainPepe <97753402+IamCaptainPepe@users.noreply.github.com>
2024-10-23 21:47:28 +0300

    run.sh

--- run.sh
+++ run.sh
@@ -3,40 +3,37 @@
 # Название виртуального окружения
 VENV_NAME="myenv"
 
-# Обновляем систему и устанавливаем необходимые пакеты
-sudo apt update
-
-# Проверяем, установлен ли Python 3, и устанавливаем его, если нет
+# Проверяем, установлен ли Python
 if ! command -v python3 &>/dev/null; then
-    echo "Python3 не установлен. Устанавливаем Python3..."
-    sudo apt install -y python3 python3-venv
+    echo "Python3 не установлен. Пожалуйста, установите Python3."
+    sudo apt update && sudo apt install -y python3 python3-pip
     if [ $? -ne 0 ]; then
         echo "Ошибка установки Python3."
         exit 1
     fi
 fi
 
-# Проверяем, установлен ли pip, и устанавливаем его, если нет
-if ! command -v pip3 &>/dev/null; then
+# Проверяем, установлен ли pip
+if ! command -v pip &>/dev/null; then
     echo "pip не установлен. Устанавливаем pip..."
     sudo apt install -y python3-pip
-    if [ $? -ne 0 ]; then
-        echo "Ошибка установки pip."
-        exit 1
-    fi
 fi
 
-# Проверяем, установлен ли screen, и устанавливаем его, если нет
+# Проверяем, установлен ли screen и устанавливаем его, если нет
 if ! command -v screen &>/dev/null; then
     echo "screen не установлен. Устанавливаем screen..."
-    sudo apt install -y screen
+    sudo apt update && sudo apt install -y screen
     if [ $? -ne 0 ]; then
         echo "Ошибка установки screen."
         exit 1
     fi
 fi
 
-# Создаем виртуальное окружение, если его нет
+# Закрываем все screen-сессии с именем monitor_session
+echo "Закрываем все существующие screen-сессии с именем monitor_session..."
+screen -ls | grep monitor_session | awk '{print $1}' | xargs -n 1 screen -S {} -X quit
+
+# Создаем виртуальное окружение
 if [ ! -d "$VENV_NAME" ]; then
     echo "Создаем виртуальное окружение..."
     python3 -m venv "$VENV_NAME"
@@ -65,6 +62,6 @@ fi
 
 # Запуск monitor.py в screen
 echo "Запускаем monitor.py в screen..."
-screen -dmS monitor_session bash -c "python3 monitor.py; exec bash"
+screen -dmS monitor_session bash -c "python monitor.py; exec bash"
 
 echo "Настройка завершена. Виртуальное окружение $VENV_NAME активировано и monitor.py запущен в screen сессии 'monitor_session'."
