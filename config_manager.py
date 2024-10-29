c7068377fa52bea4f54a4bbe8c9f6db82ffdaaa0
CaptainPepe <97753402+IamCaptainPepe@users.noreply.github.com>
2024-10-29 11:46:46 +0300

    Update config_manager.py

--- config_manager.py
+++ config_manager.py
@@ -1,5 +1,3 @@
-import json
-
 class ConfigManager:
     def __init__(self, config_paths, makefile_path):
         self.config_paths = config_paths
@@ -8,21 +6,27 @@ class ConfigManager:
     def update_private_key(self, new_private_key):
         # Обновление приватного ключа в JSON конфигурационных файлах
         for path in self.config_paths:
-            with open(path, 'r') as file:
-                config = json.load(file)
-            if "chain" in config and "wallet" in config["chain"]:
-                config["chain"]["wallet"]["private_key"] = new_private_key
-                print(f"Приватный ключ обновлен в файле: {path}")
-            with open(path, 'w') as file:
-                json.dump(config, file, indent=4)
+            try:
+                with open(path, 'r') as file:
+                    config = json.load(file)
+                if "chain" in config and "wallet" in config["chain"]:
+                    config["chain"]["wallet"]["private_key"] = new_private_key
+                    with open(path, 'w') as file:
+                        json.dump(config, file, indent=4)
+                    print(f"Приватный ключ обновлен в файле: {path}")
+            except Exception as e:
+                print(f"Ошибка при обновлении приватного ключа в файле {path}: {str(e)}")
 
         # Обновление приватного ключа в Makefile
-        with open(self.makefile_path, 'r') as file:
-            lines = file.readlines()
-        with open(self.makefile_path, 'w') as file:
-            for line in lines:
-                if line.startswith('sender :='):
-                    file.write(f'sender := {new_private_key}\n')
-                else:
-                    file.write(line)
-        print(f"Приватный ключ обновлен в файле: {self.makefile_path}")
+        try:
+            with open(self.makefile_path, 'r') as file:
+                lines = file.readlines()
+            with open(self.makefile_path, 'w') as file:
+                for line in lines:
+                    if line.startswith('sender :='):
+                        file.write(f'sender := {new_private_key}\n')
+                    else:
+                        file.write(line)
+            print(f"Приватный ключ обновлен в файле: {self.makefile_path}")
+        except Exception as e:
+            print(f"Ошибка при обновлении приватного ключа в файле {self.makefile_path}: {str(e)}")
