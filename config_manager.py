3b12b309384c8fd26e514637b472aa394e2b765c
CaptainPepe <97753402+IamCaptainPepe@users.noreply.github.com>
2024-10-29 11:35:13 +0300

    Update config_manager.py

--- config_manager.py
+++ config_manager.py
@@ -1,10 +1,12 @@
 import json
 
 class ConfigManager:
-    def __init__(self, config_paths):
+    def __init__(self, config_paths, makefile_path):
         self.config_paths = config_paths
+        self.makefile_path = makefile_path
 
     def update_private_key(self, new_private_key):
+        # Обновление приватного ключа в JSON конфигурационных файлах
         for path in self.config_paths:
             with open(path, 'r') as file:
                 config = json.load(file)
@@ -13,3 +15,14 @@ class ConfigManager:
                 print(f"Приватный ключ обновлен в файле: {path}")
             with open(path, 'w') as file:
                 json.dump(config, file, indent=4)
+
+        # Обновление приватного ключа в Makefile
+        with open(self.makefile_path, 'r') as file:
+            lines = file.readlines()
+        with open(self.makefile_path, 'w') as file:
+            for line in lines:
+                if line.startswith('sender :='):
+                    file.write(f'sender := {new_private_key}\n')
+                else:
+                    file.write(line)
+        print(f"Приватный ключ обновлен в файле: {self.makefile_path}")
